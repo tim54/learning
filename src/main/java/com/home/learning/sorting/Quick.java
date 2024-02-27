f20771e626640fc5c537e96e3f70ea2e8f9e57ee
artemkovalesvky <artem.kovalevsky@reltio.com>
2024-02-27 00:11:46 +0000

    27-Feb

--- src/main/java/com/home/learning/sorting/Quick.java
+++ src/main/java/com/home/learning/sorting/Quick.java
@@ -5,27 +5,49 @@ public class Quick {
     private Quick() { }
 
     public static void sort(Comparable a[]){
-        int n = a.length;
-        sort(a, 0, n - 1);
+        // shufle
+        sort(a, 0, a.length);
     }
 
     public static void sort(Comparable[] a, int lo, int hi){
+        if (hi <= lo) return;
+
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+
+    public static int partition(Comparable[] a, int lo, int hi){
         int i = lo;
         int j = hi + 1;
         Comparable v = a[lo];
+
+        while (true){
+
+            while (less(a[++i], v)){
+                if(i == hi) break;
+            }
+
+            while (less(v, a[--j])){
+                if (j == lo) break;
+            }
+
+            if (i >= j) break;
+
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+
+        return j;
     }
 
-    public static int partition(Comparable[] a, int hi, int lo){
-
-    }
-
-    private static boolean less(Comparable a, Comparable b){
-        return a.compareTo(b) < 0;
+    private static boolean less(Comparable v, Comparable w){
+        return v.compareTo(w) < 0;
     }
 
     private static void exch(Comparable[] a, int i, int j){
-        Comparable tmp = a[i];
+        Comparable swap = a[i];
         a[i] = a[j];
-        a[j] = a[i];
+        a[j] = swap;
     }
 }
--- src/main/java/com/home/learning/structures/Queue.java
+++ src/main/java/com/home/learning/structures/Queue.java
@@ -1,16 +1,17 @@
 package com.home.learning.structures;
 
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class Queue<Item> {
+public class Queue<Item> implements Iterable<Item>{
 
-    Node<Item> first;
-    Node<Item> last;
-    int n;
+    private Node<Item> first;
+    private Node<Item> last;
+    private int n;
 
     private static class Node<Item>{
-        Item item;
-        Node<Item> next;
+        private Item item;
+        private Node<Item> next;
     }
 
     public Queue(){
@@ -23,24 +24,57 @@ public class Queue<Item> {
 
     public int size() { return n; }
 
-    public Item peek() { return last.item; }
+    public Item peek() {
+        if (isEmpty()) throw new NoSuchElementException("");
+        return first.item;
+    }
 
     public void enqueue(Item item){
         Node<Item> oldLast = last;
-        last = new Node<>();
+        last = new Node<Item>();
 
         last.item = item;
         last.next = null;
-        oldLast.next = last;
+        if (isEmpty()) first = last;
+        else oldLast.next = last;
+        n++;
     }
 
     public Item dequeue(){
         if (isEmpty()) throw new NoSuchElementException("Queue is empty");
         Item item = first.item;
-
         first = first.next;
-
+        n--;
+        if (isEmpty()) last = null;
         return item;
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+
+        for (Item item : this){
+            s.append(item + " ");
+        }
+        return s.toString();
+    }
+
+    public Iterator<Item> iterator() { return new ListIterator<Item>(first); }
+
+    private class ListIterator<Item> implements Iterator<Item>{
+        Node<Item> current;
+
+        public ListIterator(Node<Item> first) { current = first; }
+
+        public boolean hasNext() { return current != null; }
+        public void remove() { throw new UnsupportedOperationException(); }
+
+        public Item next(){
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next;
+
+            return item;
+        }
     }
 
 }
